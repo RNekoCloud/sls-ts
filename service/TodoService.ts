50f295273be92d61224cc8a4a99f2edf8475b662
RageNeko26 <rneko2006@gmail.com>
2024-01-26 16:01:20 +0700

    feat: Initialize service pattern

---
+++ service/TodoService.ts
@@ -0,0 +1,29 @@
+import { ScanCommand } from "@aws-sdk/client-dynamodb";
+import { Request } from "express";
+import { Dyno } from "../config/dyno";
+
+class TodoService {
+    body: Request["body"];
+    params: Request["params"]
+    dyno: Dyno
+
+    constructor(req: Request, dyno: Dyno) {
+        this.body = req.body;
+        this.params = req.params;
+        this.dyno = dyno;
+    };
+
+    getAll = async() => {
+        const table = this.dyno.tableName;
+        const client = this.dyno.client;
+
+        const cmd = new ScanCommand({
+            TableName: table
+        });
+
+        const response = await client.send(cmd)
+        return response.Items
+    };
+};
+
+export default TodoService;
