347bf057eb6d18ed7d1a16f3c1c07b6b739e489f
RageNeko26 <rneko2006@gmail.com>
2024-01-26 16:00:15 +0700

    fix: Separate config types

--- config/dyno.ts
+++ config/dyno.ts
@@ -1,6 +1,13 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
-export const Dyno = new DynamoDBClient({
-    // Setup local DynamoDB
-    endpoint: "http://localhost:8000"
-})
+export interface Dyno {
+    client: DynamoDBClient,
+    tableName: string, 
+}
+
+export const Local: Dyno = {
+    client: new DynamoDBClient({
+        endpoint: "http://localhost:3000"
+    }),
+    tableName: "todo"
+}
