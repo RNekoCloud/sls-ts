fadac785ef13e2b4342a3383693d2e202471c4ef
RageNeko26 <rneko2006@gmail.com>
2024-01-28 22:31:53 +0700

    feat: Initialize entry point

--- index.ts
+++ index.ts
@@ -2,6 +2,19 @@ const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
 
+import { Local } from "./config/dyno"
+import TodoRoutes from "./routes/TodoRoute";
+import TodoService from "./service/TodoService";
+import TodoController from "./controller/TodoController";
+
+// Dependency injection
+const todoService = new TodoService(Local);
+const todoController = new TodoController(todoService);
+const todoRoutes = new TodoRoutes(todoController)
+
+// Initialize routes
+todoRoutes.setup(app)
+
 app.get("/", (req, res, next) => {
   return res.status(200).json({
     message: "Hello from root!",
