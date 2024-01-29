1afc4e0fa9886431dc714cece0cbd62027ea2dc9
RageNeko26 <rneko2006@gmail.com>
2024-01-29 14:43:00 +0700

    feat: Add body parser

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 const serverless = require("serverless-http");
 const express = require("express");
 const app: Express = express();
+const bodyParser = require("body-parser");
 
 // Setup Routes and Controller
 import TodoController from "./controller/TodoController";
@@ -9,6 +10,9 @@ import TodoRoutes from "./routes/TodoRoute";
 
 const todoController = new TodoController();
 const todoRoutes = new TodoRoutes(todoController);
+
+// Setup parser
+app.use(bodyParser.json)
 
 todoRoutes.setup(app);
 
