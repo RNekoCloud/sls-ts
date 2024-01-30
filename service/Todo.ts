e1b1593ff758873dec54050f2d00eb8d1eeb1a37
RageNeko26 <rneko2006@gmail.com>
2024-01-30 23:29:00 +0700

    feat: Add raw data interface

--- service/Todo.ts
+++ service/Todo.ts
@@ -1,4 +1,18 @@
 export interface Todo {
+    id: string,
     title: string,
     status: string,
+}
+
+// Result query of DynamoDB
+export interface RawTodo {
+    id: {
+        S: string
+    },
+    title: {
+        S: string,
+    },
+    status: {
+        S: string,
+    }
 }
