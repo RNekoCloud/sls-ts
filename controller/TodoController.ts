7bbe5513f203c83d41468aca8c0fa9143ffde3bb
RageNeko26 <rneko2006@gmail.com>
2024-01-30 23:29:45 +0700

    fix: Dynamically generate ID in controller request

--- controller/TodoController.ts
+++ controller/TodoController.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import TodoService from "../service/TodoService";
 import { Local } from "../config/dyno";
 import { Todo } from "../service/Todo";
+import { nanoid } from "nanoid";
 
 class TodoController  {
     public async getAll(req: Request, res: Response, next: NextFunction ): Promise<Response> {
@@ -17,6 +18,7 @@ class TodoController  {
     public async save(req: Request, res: Response, next: NextFunction): Promise<Response> {
         let service: TodoService = new TodoService(Local)
         const data: Todo = {
+            id: nanoid(),
             title: req.body.title,
             status: req.body.status,
         }
